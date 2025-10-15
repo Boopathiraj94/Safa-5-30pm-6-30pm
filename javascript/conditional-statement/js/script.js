2d6d49be05ddbfa942437a4e186f1a92f7c9703f
Boopathiraj94 <boopathirajk55@gmail.com>
2025-10-15 13:33:10 +0530

    operator if ,if-else

---
+++ javascript/conditional-statement/js/script.js
@@ -0,0 +1,34 @@
+
+/*
+if(condition){
+  // block of code
+}
+
+student 50 and above  >= 50
+
+if(condition){
+
+}else{
+
+    }
+
+*/
+
+// let mark = prompt("Enter your mark?")
+// debugger;
+// if(mark >=50){
+//     document.writeln("<h1>You are Pass</h1>")
+// }else{
+//     document.writeln("<h1>You are fail , sorry</h1>")
+// }
+
+// odd even
+// driving licence = >  17 and above
+// negative or positive number
+// 
+
+let mark = prompt("enter your mark?")
+debugger;
+// (condition) ? true block : false block ;
+(mark >=50) ? document.writeln("<h1>You are Pass</h1>")  : document.writeln("<h1>You are fail , sorry</h1>");
+
---
+++ javascript/operators/js/scripts.js
@@ -0,0 +1,67 @@
+// let a = 5;
+
+//  +=
+// let b = 5;
+//   b += 3; 
+//  b = b +3;  
+//  b = 5 + 3;
+// b = 8
+// console.log(b)
+// let c = 10;
+// let d = 3;
+
+//   c+=d; // 13
+//   ++c; // 14
+//   --d; // 2
+//   console.log(c - d); // 14- 2 => 12
+
+//  c+=d;
+// c = c + d;
+// c = 10 + 3;
+// c = 13;
+// console.log(d)
+// c -=d;
+// console.log(c)
+// c = c -d; => c = 10 - 3; c= 7
+// c *=d;
+// console.log(c)
+// c = c* d=> c = 10*3 => c = 30
+// c /=d;
+// console.log(c)
+// c= c / d => c = 10/3 => c = 3
+// c %=d;
+// console.log(c)
+// c = c%d => c = 10%3 => c = 1
+
+// comparision operator
+//  == , === , != ,>, < , >=,<= , 
+
+// console.log("5 == 6",5 == 6) ; // true
+// console.log("6 === 5",6 === '5') ;  // false
+//  == only value
+//  === value and datatype
+
+// != vs !==
+//  != only value
+//  !== value and datatype
+// console.log(5 != 5);
+//  console.log(5 !== '5');
+
+// console.log(5 > 2);
+// console.log(5 > 5);
+// console.log(2 < 5);
+// console.log(5 <= 5);
+// console.log(5 >= 5);
+
+let a = 7
+let b = 6
+// && , || , !
+console.log(a == b && a >= b); // false && false 
+console.log(a == b || a >= b); 
+
+// not
+console.log(!true)
+console.log(!false)
+
+
+ 
