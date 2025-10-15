ebbbd87d10c11ecbd2c62b594e37955e41fdf202
Boopathiraj94 <boopathirajk55@gmail.com>
2025-10-15 12:38:27 +0530

    intro

---
+++ javascript/intro/js/script.js
@@ -0,0 +1,35 @@
+// alert("I am from External js")
+
+// output
+
+// alert()
+// document.write()
+// console.log()
+// print()
+
+// console.log("hi welcome")
+
+// input
+// prompt("Enter your Name?")
+
+// confirm("Do you want to Logout?")
+
+ name = "Safa";
+ name = 22
+ name = 22
+ name = 22
+ name = 22
+ name1 = "sana"
+
+//  nam2= "siva"
+//  nnnn = "df"
+//  1322 = "sdfsd"
+
+//  name123423 ="sdfsdf"
+//  2153764name ="sdfsd"
+
+ _ ="dsfsdf"
+ $$$$$$$$$ ="dsfsdf"
+ 
+
+ console.log(_);
