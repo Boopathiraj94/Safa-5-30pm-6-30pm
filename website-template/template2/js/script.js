0a738ec1be888c9439ab667572247a94ce7baf4c
Boopathiraj94 <boopathirajk55@gmail.com>
2025-10-25 19:09:04 +0530

    dom addto count

---
+++ website-template/template2/js/script.js
@@ -0,0 +1,12 @@
+// alert("hi")
+// let a = document.getElementById("logo").innerHTML;
+
+// console.log(a)
+
+let addToCart = ()=>{
+   let c = document.getElementById("addtocartcount");
+    let count = Number(c.innerText)
+    count+=1;
+    c.innerText = count
+    // alert("sdhisdf")
+}
