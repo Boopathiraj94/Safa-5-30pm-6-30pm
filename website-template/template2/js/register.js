1bf76c004b829f00a6796274aeed515df1962278
unknown <boopathirajk55@gmail.com>
2025-10-26 19:00:30 +0530

    register form / object completed / form-validation / object generate / input value taken

---
+++ website-template/template2/js/register.js
@@ -0,0 +1,57 @@
+let register_form = () => {
+
+    let uname = document.getElementById("uname");
+    let email = document.getElementById("email");
+    let pass = document.getElementById("pass");
+
+    console.log("uname", uname.value);
+    console.log("email", email.value);
+    console.log("pass", pass.value);
+
+
+    let isValid = false;
+
+    if (uname.value === "") {
+        document.getElementById("unameError").innerText = "Enter your user name"
+        isValid = false;
+        // return;
+    } else {
+        document.getElementById("unameError").innerText = ""
+        isValid = true;
+    }
+
+    if (email.value === "") {
+        document.getElementById("emailError").innerText = "Enter your Email id"
+        isValid = false;
+        // return;
+    } else {
+        document.getElementById("emailError").innerText = ""
+        isValid = true;
+    }
+
+
+    if (pass.value === "") {
+        document.getElementById("passError").innerText = "Enter your password"
+        isValid = false;
+        // return;
+    } else {
+        document.getElementById("passError").innerText = ""
+        isValid = true;
+    }
+
+
+    if (isValid) {
+        let user_details = {
+            user_name: uname.value,
+            email_id: email.value,
+            password: pass.value
+        };
+
+        console.log(user_details)
+    }else{
+        console.log("object is not generate")
+    }
+
+
+
+}
